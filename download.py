52c98c983bb94872b05347c41c8f127ab2feb879
akasakaid <fawwazthoerif@gmail.com>
2022-01-04 13:38:40 +0700

    update

--- download.py
+++ download.py
@@ -15,12 +15,18 @@ r = requests.Session()
 def download(url):
 	try:
 		req = r.get(url)
+		var_n = eval(re.search('var n = (.*?);',req.text).group(1))
+		var_b = eval(re.search('var b = (.*?);',req.text).group(1))
+		var_z = eval(re.search('var z = (.*?);',req.text).group(1))
 		origin = re.search('https://(.*?)/',url).group(1)
+		
 		elemen = re.search('document.getElementById\(\'dlbutton\'\).href = \"(.*?)\" \+ \((.*?)\) \+ \"(.*?)\";',req.text)
-		matematika = eval(elemen.group(2))
+		elemen = re.search('document.getElementById\(\'dlbutton\'\).href = "(.*?)"\+\((.*?)\)\+\"(.*?)\";',req.text)
+		matematika = var_n + var_b + var_z - 3
 		url_download = "https://" + origin + elemen.group(1) + str(matematika) + elemen.group(3)
 		print("# redirect to browser !")
 		click.launch(url_download)
+		print(url_download)
 	except ValueError:
 		exit(f"# failed download from {url}")
 
